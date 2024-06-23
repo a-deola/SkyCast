c6b99b209b2bae657b1115f377440d3a265affd6
Adeola Samuel <meetprecious1403@gmail.com>
2024-06-23 08:09:54 +0100

    modify weather model

--- server/models/weather.go
+++ server/models/weather.go
@@ -6,10 +6,21 @@ import (
 
 type Weather struct {
 	gorm.Model
-	City        string
-	Temperature float64
-	Condition   string
 	Lon         float64
 	Lat         float64
+	Condition   string
 	Icon        string
+	Description string
+	Rain        float64
+	Temperature float64
+	Feels_Like  float64
+	Temp_Min    float64
+	Temp_Max    float64
+	Pressure    int
+	Humidity    int
+	Visibility  int
+	WindSpeed   float64
+	Country     string
+	Timezone    int
+	City        string
 }
