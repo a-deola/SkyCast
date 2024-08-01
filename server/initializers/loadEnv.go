dc458b5e9598e838fa799a104b00666bb90a8b62
Adeola Samuel <meetprecious1403@gmail.com>
2024-08-01 22:23:03 +0100

    env error fix

--- server/initializers/loadEnv.go
+++ server/initializers/loadEnv.go
@@ -1,16 +1,23 @@
 package initializers
 
 import (
-	"fmt"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println(err)
-		log.Fatal("Error loading .env file")
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
+	db := os.Getenv("DB_DSN")
+	api := os.Getenv("WEATHER_API_KEY")
+	if db == "" || api == "" {
+		return
+	} else {
+		godotenv.Load()
 	}
+	log.Println("DB_DSN: ", db)
 }
