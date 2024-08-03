072b33b52f802192ba90b8d3214c23b1896a0b6b
Adeola Samuel <meetprecious1403@gmail.com>
2024-08-03 22:00:22 +0100

    chore: updated workflow

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/SkyCast/client",
+  base: "/SkyCast/",
   plugins: [react()],
 });
