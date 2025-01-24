4998fbb2fc80cfe779246e28eb2af8d9b0c58158
Adeola Samuel <meetprecious1403@gmail.com>
2025-01-24 10:56:59 +0100

    define colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,21 +1,20 @@
-import daisyui from 'daisyui';
+import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      boxShadow:{
-        'outer':'10px 10px 50px rgba(204, 197, 185, 0.5)',
-        'inner':'inset 10px 10px 50px rgba(204, 197, 185, 0.5)',
-      }
+      boxShadow: {
+        outer: "10px 10px 50px rgba(204, 197, 185, 0.5)",
+        inner: "inset 10px 10px 50px rgba(204, 197, 185, 0.5)",
+      },
+      colors: {
+        primary: "#EB5E28", // Primary color
+        muted: "#CCC5B9", // Accent color for details
+      },
     },
   },
-  plugins: [
-  ],
-  darkMode:'class'
-}
-
+  plugins: [],
+  darkMode: "class",
+};
